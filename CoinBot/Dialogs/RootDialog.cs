90f77c2855387a3d2e654d6391a2f27a8c02a902
Yura BLACK <yura43528@gmail.com>
2017-11-14 20:29:59 +0200

    Добавьте файлы проекта.

---
+++ CoinBot/Dialogs/RootDialog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace CoinBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(this.ActionRevivedAsync);
+
+            return Task.CompletedTask;
+        }
+        private async Task ActionRevivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            Activity activity = await result as Activity;
+            if (activity.ToString() == ActivityTypes.Typing)
+            {
+                await context.PostAsync($"You are typing");
+            }
+            // calculate something for us to return
+            // int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            // await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            Activity activity = await result as Activity;
+            if (activity.ToString() == ActivityTypes.Typing)
+            {
+                await context.PostAsync($"You are typing");
+            }
+            // calculate something for us to return
+            // int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            // await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
